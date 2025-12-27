44b69467b5484a3a934fb58d97736a65b86b725a
saneshps <74853379+saneshps@users.noreply.github.com>
2025-12-27 15:07:28 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,76 @@
+
+<section id="footer">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-3 col-sm-4">
+				<h3><span>Our</span> Links</h3>
+					<a href="van-racking.php"><p>Products</p></a>
+					<a href="#"><p>Installation</p></a>
+					<a href="about.php"><p>About</p></a> 
+					<a href="news.php"><p>News & Resources</p></a>					
+					<!--<a href="#"><p>Tech Blogs</p></a> -->
+					<a href="contact.php"><p>Contact us</p></a>
+			</div>
+
+
+			
+
+			<div class="col-md-3 col-sm-4 add">
+				
+					<!-- <h3>stroetogo Floor</h3> -->
+<!--					<p>OFFICE NO. LV-27D,<br>-->
+<!--PO BOX 42167,</p>-->
+<!--					<p>HAMRIYAH FREE ZONE-->
+<!--PHASE 2,</p>-->
+<!--					<p>SHARJAH | UAE</p>-->
+				
+
+		
+				
+<!--					<p>TEL : +971 6 526 4382</p>-->
+<!--					<p>MOB : +971 54 791 8858</p>-->
+<!--					<p>FAX : +971 6 526 4384</p>-->
+					<p>Mail : <a href="mailto:sales@storetogo.ae">sales@storetogo.ae</a></p>
+			</div>
+
+
+ <div class="col-md-6 col-sm-4 ">
+				
+					<img src="images/logo2.png" class="img-responsive foot-logo" alt=""/>
+			</div> 
+
+
+
+
+
+		</div>
+
+	</div>
+
+</section>
+
+
+<section id="rated-main">	
+				<div class="container-fluid">
+					<div class="row">
+					</div>
+				</div>
+
+
+			<section id="back">
+				<div class="container-fluid">
+					<div class="row">
+					<div class="col-md-7 col-sm-9">
+						<p>© 2025  <span>www.storetogo.ae</span> All rights reserved.  Powered By  <a href="http://bigleap.ae/" target="blank"><span>BigLeap</span></a></p>
+							</div>
+
+						<div class="col-md-5 col-sm-3">				
+					<a href="https://www.instagram.com/store.togo/?igshid=ky6qdpr4zggs" target="blank"><i class="demo-icon icon-instagram">&#xe812;</i></a>
+					<a href="https://www.linkedin.com/company/storetogo/" target="blank"><i class="demo-icon icon-linkedin">&#xf0e1;</i></a>
+					
+					<a href="https://www.facebook.com/Storetogo-103017204876561/" target="blank"><i class="demo-icon icon-003-facebook">&#xe808;</i></a>
+				</div>
+				</div>
+			</div>
+	</section>
+</section>
